ce8ebf3b8c1d9c0cf599c37b041d3cbb7a19ed74
Mayank <mayank.gupta@iongroup.com>
2020-05-04 21:18:43 +0530

    Changes

---
+++ src/container/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container-fluid application-header">
+      <h1>Employee Management System...</h1>
+    </div>
+  );
+}
+
+export default App;
